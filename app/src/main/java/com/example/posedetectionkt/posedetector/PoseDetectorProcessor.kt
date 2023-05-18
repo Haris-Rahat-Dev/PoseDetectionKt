602385c891e6cde12a8dc6ef06352eec90e9e9a3
Haris-bin-Rahat <81488844+Rage-coder69@users.noreply.github.com>
2023-05-18 21:53:29 +0500

    completed with pointnet classification

--- app/src/main/java/com/example/posedetectionkt/posedetector/PoseDetectorProcessor.kt
+++ app/src/main/java/com/example/posedetectionkt/posedetector/PoseDetectorProcessor.kt
@@ -6,19 +6,17 @@ import android.graphics.Paint
 import android.media.AudioManager
 import android.media.ToneGenerator
 import android.util.Log
-import com.example.posedetectionkt.utils.preference.UserDetails
+import com.example.posedetectionkt.ml.Pointnet
 import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseDetector
 import com.google.mlkit.vision.pose.PoseDetectorOptionsBase
-import com.google.mlkit.vision.pose.PoseLandmark
-import kotlin.math.abs
-import kotlin.math.atan2
-import kotlin.math.roundToInt
-import kotlin.math.roundToLong
-import kotlin.math.sqrt
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.IOException
+import java.nio.ByteBuffer
 
 class PoseDetectorProcessor(
     private val context: Context,
@@ -31,8 +29,16 @@ class PoseDetectorProcessor(
     private val paintColor: Paint
     private var reps: Int = 0
     private var stage: String = "none"
+    private var lastStage: String = "none"
     val toneGen: ToneGenerator = ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-
+    private lateinit var model: Pointnet
+    private lateinit var inputFeature0: TensorBuffer
+    private val classMap = mapOf(
+        0 to "pushup_down",
+        1 to "pushup_up",
+        2 to "squat_down",
+        3 to "squat_up"
+    )
 
     init {
         detector = PoseDetection.getClient(options)
@@ -40,11 +46,33 @@ class PoseDetectorProcessor(
         paintColor = Paint()
         paintColor.strokeWidth = STROKE_WIDTH
         paintColor.textSize = IN_FRAME_LIKELIHOOD_TEXT_SIZE
+        // set the stage and last stage based on the pose selected
+        when (pose) {
+            "pushup" -> {
+                stage = "pushup_up"
+                lastStage = "pushup_down"
+            }
+
+            "squat" -> {
+                stage = "squat_up"
+                lastStage = "squat_down"
+            }
+        }
+        try {
+            model = Pointnet.newInstance(context)
+            // Creates inputs for reference.
+            inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 33, 3), DataType.FLOAT32)
+            // Releases model resources if no longer used.
+        } catch (exception: IOException) {
+            Log.d("PoseDetectorProcessor", "Error: ${exception.message}")
+        }
+
     }
 
     override fun stop() {
         super.stop()
         detector.close()
+        model.close()
     }
 
     override fun detectInImage(image: InputImage): Task<Pose> {
@@ -53,149 +81,75 @@ class PoseDetectorProcessor(
                 it.result
             }
     }
-    
-
-    // write a function for calculating distance between two points
-    private fun getDistance(
-        firstPoint: PoseLandmark,
-        lastPoint: PoseLandmark
-    ): Double {
-        val x = firstPoint.position3D.x - lastPoint.position3D.x
-        val y = firstPoint.position3D.y - lastPoint.position3D.y
-        return sqrt((x * x + y * y).toDouble())
-    }
 
 
     override fun onSuccess(results: Pose, graphicOverlay: GraphicOverlay) {
         if (results.allPoseLandmarks.size > 0) {
-            // TODO: classify poses using angle heuristics for pushups, squats and plank
-            if (this.pose == "pushup") {
+            val bufferCapacity =
+                results.allPoseLandmarks.size * 3 * Float.SIZE_BYTES // Assuming 2 floats per landmark (x, y)
+            val byteBuffer = ByteBuffer.allocate(bufferCapacity)
+            val poseLandmarks = results.allPoseLandmarks
+            for (landmark in poseLandmarks) {
+                // Extract relevant information from the pose landmarks
+                val landmarkX = landmark.position3D.x
+                val landmarkY = landmark.position3D.y
+                val landmarkZ = landmark.position3D.z
 
-                val shoulder = results.allPoseLandmarks[PoseLandmark.LEFT_SHOULDER]
-                val hip = results.allPoseLandmarks[PoseLandmark.LEFT_HIP]
-                val ankle = results.allPoseLandmarks[PoseLandmark.LEFT_ANKLE]
-
-                val angle = getAngle(shoulder, hip, ankle)
-
-                Log.d("PushUpAngle", angle.toString())
-
-                if (angle in 160.0..190.0) {
-                    val elbow = results.allPoseLandmarks[PoseLandmark.LEFT_ELBOW]
-                    val wrist = results.allPoseLandmarks[PoseLandmark.LEFT_WRIST]
-
-                    val angle2 = getAngle(shoulder, elbow, wrist)
-
-                    if (angle2 in 160.0..190.0) {
-                        Log.d("POSE", "Push_up")
-                        stage = "up"
-                        paintColor.color = Color.GREEN
-                        graphicOverlay.add(
-                            PoseGraphic(
-                                graphicOverlay,
-                                results,
-                                paintColor,
-                                reps.toString(),
-                                stage
-                            )
-                        )
-                    }
-
-                    if (angle2 in 30.0..90.0 && stage == "up") {
-                        Log.d("POSE", "Push_down")
-                        stage = "down"
-                        reps += 1
-                        toneGen.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
-                        paintColor.color = Color.GREEN
-                        graphicOverlay.add(
-                            PoseGraphic(
-                                graphicOverlay,
-                                results,
-                                paintColor,
-                                reps.toString(),
-                                stage
-                            )
-                        )
-                    }
-
-                } else {
-                    paintColor.color = Color.RED
-                    graphicOverlay.add(
-                        PoseGraphic(
-                            graphicOverlay,
-                            results,
-                            paintColor,
-                            reps.toString(),
-                            stage
-                        )
-                    )
-                }
-            } else if (this.pose == "squat") {
-
-                val shoulder = results.allPoseLandmarks[PoseLandmark.LEFT_SHOULDER]
-
-                val hip = results.allPoseLandmarks[PoseLandmark.LEFT_HIP]
-
-                val ankle = results.allPoseLandmarks[PoseLandmark.LEFT_ANKLE]
-
-                val angle = getAngle(shoulder, hip, ankle)
-
-                val shoulder_distance =
-                    getDistance(shoulder, results.allPoseLandmarks[PoseLandmark.RIGHT_SHOULDER])
-                val ankle_distance =
-                    getDistance(
-                        ankle,
-                        results.allPoseLandmarks[PoseLandmark.RIGHT_ANKLE]
-                    ).roundToInt()
-                val min_shoulder_distance =
-                    (shoulder_distance - shoulder_distance * 0.4).roundToInt()
-                val max_shoulder_distance =
-                    (shoulder_distance + shoulder_distance * 0.4).roundToInt()
-
-
-                if (ankle_distance in min_shoulder_distance..max_shoulder_distance) {
-                    if (angle in 160.0..190.0) {
-                        Log.d("POSE", "Squatup")
-                        stage = "up"
-                        paintColor.color = Color.GREEN
-                        graphicOverlay.add(
-                            PoseGraphic(
-                                graphicOverlay,
-                                results,
-                                paintColor,
-                                reps.toString(),
-                                stage
-                            )
-                        )
-                    }
-
-                    if (angle in 30.0..90.0 && stage == "up") {
-                        Log.d("POSE", "Squatdown")
-                        reps += 1
-                        toneGen.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
-                        paintColor.color = Color.GREEN
-                        graphicOverlay.add(
-                            PoseGraphic(
-                                graphicOverlay,
-                                results,
-                                paintColor,
-                                reps.toString(),
-                                stage
-                            )
-                        )
-                    }
-                } else {
-                    paintColor.color = Color.RED
-                    graphicOverlay.add(
-                        PoseGraphic(
-                            graphicOverlay,
-                            results,
-                            paintColor,
-                            reps.toString(),
-                            stage
-                        )
-                    )
+                // Assuming `byteBuffer` is a `ByteBuffer` with the appropriate capacity
+                // and that `float` values are being stored in the buffer
+                byteBuffer.putFloat(landmarkX)
+                byteBuffer.putFloat(landmarkY)
+                byteBuffer.putFloat(landmarkZ)
+            }
+            inputFeature0.loadBuffer(byteBuffer)
+            // Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+            val predictions = FloatArray(outputFeature0.shape[1])
+            outputFeature0.floatArray.copyInto(predictions)
+            // Find the index of the maximum predicted probability
+            var maxProbabilityIndex = 0
+            for (i in 1 until predictions.size) {
+                if (predictions[i] > predictions[maxProbabilityIndex]) {
+                    maxProbabilityIndex = i
                 }
             }
+            // Retrieve the predicted class label based on the max probability index
+            val predictedClass = classMap[maxProbabilityIndex]
+            Log.d("PoseDetectorProcessor", "Predicted class: $predictedClass")
+            if (predictedClass == pose + "_up" || predictedClass == pose + "_down") {
+                // Check if stage has changed
+                if (predictedClass != stage && predictedClass != lastStage) {
+                    stage = predictedClass
+                    reps++
+                    println("$pose count: $reps")
+                    toneGen.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+                }
+                lastStage = predictedClass
+
+                paintColor.color = Color.GREEN
+                graphicOverlay.add(
+                    PoseGraphic(
+                        graphicOverlay,
+                        results,
+                        paintColor,
+                        reps.toString(),
+                        stage
+                    )
+                )
+            } else {
+                paintColor.color = Color.GREEN
+                graphicOverlay.add(
+                    PoseGraphic(
+                        graphicOverlay,
+                        results,
+                        paintColor,
+                        reps.toString(),
+                        stage
+                    )
+                )
+            }
+
         } else {
             paintColor.color = Color.RED
             graphicOverlay.add(
